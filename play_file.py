5627d98bd59b613d42486d80c422cd1a4a597d03
gacrichards <g.a.cole.richards@gmail.com>
2023-08-08 18:47:31 -0600

    adds more sfx

--- play_file.py
+++ play_file.py
@@ -10,9 +10,11 @@ def laughing():
     play("laughing.mp3")
 def wolf():
     play("wolf-howl.mp3")
-def witch():
-    play("witch.mp3")
-
+def owl():
+    play("owl.mp3")
+def door_creek():
+    play("door-creek.mp3")
+    
 def play(sound_file):
     sound = mixer.Sound(sound_file)
     # Play the sound
